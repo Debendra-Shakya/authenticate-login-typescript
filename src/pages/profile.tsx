0d966c5682979914373496cddd70d4aa5d3caf83
debz1 <debenshakya1@gmail.com>
2022-06-28 11:20:57 +0545

    log out

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,49 +1,40 @@
-import { LoadingButton } from '@mui/lab'
-import { Typography } from '@mui/material'
-import React from 'react'
-import { Navigate, useLocation, useNavigate } from 'react-router-dom'
+import { LoadingButton } from "@mui/lab";
+import { Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 
-const profile = () => {
-  const name=localStorage.getItem('name')
+const Profile = () => {
+  const name = localStorage.getItem("name");
+  const navigate = useNavigate();
 
-  const handlesubmit = (e: any) => {
-   localStorage.clear();
-      // Navigate('/login')
-   
-  };
+  
   const RootStyle = styled("div")({
     background: "rgb(249, 250, 251)",
     height: "100vh",
     display: "grid",
     placeItems: "center",
-    
   });
   return (
     <div>
       <RootStyle>
-      <Typography variant='h2' >
-    welcome to profile page {name}
-
-   </Typography>
-   <Typography >
-   <LoadingButton
-     
-              size="large"
-              type="submit"
-              variant="contained"
-              onSubmit={handlesubmit}
-              
-            >
-             logout
-            </LoadingButton>
-
-   </Typography>
-
+        <Typography variant="h2">welcome to profile page {name}</Typography>
+        <Typography>
+          <LoadingButton
+            size="large"
+            variant="contained"
+            onClick={() => {
+              localStorage.clear();
+              alert("logged out");
+              navigate("/login");
+            }}
+          >
+            logout
+          </LoadingButton>
+        </Typography>
       </RootStyle>
-  
     </div>
-  )
-}
+  );
+};
 
-export default profile
+export default Profile;
