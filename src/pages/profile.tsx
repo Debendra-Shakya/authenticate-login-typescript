031c6af5b63c01ebcb928607341ee3f5c6d0ac64
debz1 <debenshakya1@gmail.com>
2022-06-28 11:08:32 +0545

    ui on profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,30 +1,47 @@
 import { LoadingButton } from '@mui/lab'
 import { Typography } from '@mui/material'
 import React from 'react'
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
+import styled from "@emotion/styled";
 
 const profile = () => {
   const name=localStorage.getItem('name')
-  // alert(`${name}, welcome to profile page`)
+
+  const handlesubmit = (e: any) => {
+   localStorage.clear();
+      // Navigate('/login')
+   
+  };
+  const RootStyle = styled("div")({
+    background: "rgb(249, 250, 251)",
+    height: "100vh",
+    display: "grid",
+    placeItems: "center",
+    
+  });
   return (
     <div>
-   <Typography variant='h2' align='center'>
+      <RootStyle>
+      <Typography variant='h2' >
     welcome to profile page {name}
 
    </Typography>
-   <Typography align='center'>
-
+   <Typography >
    <LoadingButton
      
               size="large"
               type="submit"
               variant="contained"
-              
+              onSubmit={handlesubmit}
               
             >
              logout
             </LoadingButton>
+
    </Typography>
 
+      </RootStyle>
+  
     </div>
   )
 }
