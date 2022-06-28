639352f8cc0c05a0761cf0f4b05ff311cd475313
debz1 <debenshakya1@gmail.com>
2022-06-28 10:53:31 +0545

    simple page profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,31 @@
+import { LoadingButton } from '@mui/lab'
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const profile = () => {
   const name=localStorage.getItem('name')
-  alert(`${name}, welcome to profile page`)
+  // alert(`${name}, welcome to profile page`)
   return (
-    <div>profile page</div>
+    <div>
+   <Typography variant='h2' align='center'>
+    welcome to profile page {name}
+
+   </Typography>
+   <Typography align='center'>
+
+   <LoadingButton
+     
+              size="large"
+              type="submit"
+              variant="contained"
+              
+              
+            >
+             logout
+            </LoadingButton>
+   </Typography>
+
+    </div>
   )
 }
 
